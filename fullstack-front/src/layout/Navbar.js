ab7aa79fa923e712b5d2c06d5dbd4a458343f465
AnshikaAgarwal10 <anshikaga10@gmail.com>
2023-05-03 18:32:12 -0400

    Front-end table

--- fullstack-front/src/layout/Navbar.js
+++ fullstack-front/src/layout/Navbar.js
@@ -1,13 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 
 export default function Navbar() {
   return (
     <div>
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
+<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a className="navbar-brand" href="#">Full Stack Application</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
+
+    <button className="btn btn-outline-light">Add Users</button>
   
 </nav>
 
